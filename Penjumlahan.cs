0e63de14fa47a7ac7bda62454905834c4617d5a2
PRAKTIKAN <udinamin445@gmail.com>
2023-03-20 07:49:56 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ Penjumlahan.cs
@@ -0,0 +1,14 @@
+﻿namespace modul5_1302210090{
+    internal class Penjumlahan<T>{
+        public void JumlahTigaAngka<T>(T angka1, T angka2, T angka3){
+
+            dynamic asi = angka1;
+            dynamic asi2 = angka2;
+            dynamic asi3 = angka3;
+
+
+            Console.WriteLine(asi + asi2 + asi3);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World! hhhhhh");
+        static void Main(string[] args){
+
+            string NIM = "1302210090";
+
+            Penjumlahan<long> jumlah = new Penjumlahan<long>();
+            jumlah.JumlahTigaAngka(21, 00, 90);
         }
     }
 }
