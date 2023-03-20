4c07c6949aa08b658d8580f78d4a6ad035134008
PRAKTIKAN <udinamin445@gmail.com>
2023-03-20 08:20:18 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace modul5_1302210090
+﻿using System.ComponentModel;
+
+namespace modul5_1302210090
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! hhhhhh");
+            SimpleDataBase<int> storedData = new SimpleDataBase<int>();
+            storedData.AddNewData(123456);
+            storedData.PrintAllData();
         }
     }
 }
---
+++ SimpleDataBase.cs
@@ -0,0 +1,26 @@
+﻿namespace modul5_1302210090
+{
+    internal class SimpleDataBase<T>{
+        private List<T> storedData;
+        private List<DateTime> date;
+
+        public SimpleDataBase()
+        {
+            storedData= new List<T>();
+            date= new List<DateTime>();
+        }
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            date.Add(DateTime.Now);
+
+        }
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine(storedData[i] + " " + date[i]);
+            }
+        }
+    }
+}
